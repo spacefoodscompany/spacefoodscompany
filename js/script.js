531a9d2a4e73d256a18522bb563339e66452c31d
spacefoodscompany <nft@spacefoodscompany.com>
2021-11-05 13:33:03 -0400

    FAQ section

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ btnNavEl.addEventListener("click", function(){
     headerEl.classList.toggle("nav-open");
 });
 
+var acc = document.getElementsByClassName("fa-plus-circle");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("open");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
